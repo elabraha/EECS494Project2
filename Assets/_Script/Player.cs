ed286f8f2f8cb33f03c5e36391cf78241467e3f1
Yuxiang Yao <yuxiangy@umich.edu>
2016-10-06 19:56:37 -0400

    add a singleton for player

--- Assets/_Script/Player.cs
+++ Assets/_Script/Player.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	Rigidbody rigid;
+	public float speed = 10f;
+	public static Player S;
 
 	// Use this for initialization
 	void Start () {
+		S = this;
 		rigid = gameObject.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			
+			transform.position += speed * Vector3.forward;
 		}
 	}
 }
