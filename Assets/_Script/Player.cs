bc9a8b0e12e7f5c6b895195339e7943250001dce
Yuxiang Yao <yuxiangy@umich.edu>
2016-10-06 19:46:06 -0400

    new project

---
+++ Assets/_Script/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			
+		}
+	}
+}
