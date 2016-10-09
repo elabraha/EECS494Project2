e30bd113c4f11d7fa8e409833a23d851cb1f9063
Yuxiang Yao <yuxiangy@umich.edu>
2016-10-09 00:20:55 -0400

    something for later

---
+++ Assets/_Script/WinnerStage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinnerStage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		// Do something to show the player passes the instruction level
+		Debug.Log("Player Win the instruction level!");
+	}
+}
