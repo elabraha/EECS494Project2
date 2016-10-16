0a8fa5474beecae75011064ffaf239bc5a1f598e
Yuxiang Yao <yuxiangy@umich.edu>
2016-10-16 01:30:14 -0400

    add an evil ball to crush playertest by KeyCode.E, may be used when time is up, player now is destructable

---
+++ Assets/_Script/Broken/BreakPlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakPlayer : MonoBehaviour {
+
+	public GameObject evil;
+
+	void Start(){
+		this.transform.FindChild ("brokenPlayer").gameObject.SetActive (false);
+		//TEST: this is just code for test
+		GameObject.Find ("Evil").SetActive(false);
+	}
+
+	void FixedUpdate(){
+		//TEST: this is just code for test
+		if(Input.GetKey(KeyCode.E)){
+			float _y = evil.gameObject.transform.position.y;
+			evil.SetActive(true);
+			evil.transform.position = new Vector3 (this.gameObject.transform.position.x, _y, this.transform.position.z);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Evil" 
+			&& other.gameObject.transform.position.y > this.transform.position.y) {
+			this.transform.FindChild ("brokenPlayer").gameObject.SetActive (true);
+			this.transform.FindChild ("brokenPlayer").gameObject.transform.position 
+				= this.transform.FindChild ("Player").gameObject.transform.position;
+			Destroy (this.transform.FindChild ("Player").gameObject.GetComponent<SphereCollider>());
+			Destroy (this.gameObject.GetComponent<SphereCollider>());
+			this.transform.FindChild ("Player").gameObject.SetActive (false);
+			foreach (Rigidbody rgd in this.transform.FindChild("brokenPlayer").gameObject.GetComponentsInChildren<Rigidbody>()) {
+				rgd.constraints = RigidbodyConstraints.None;
+				rgd.velocity = new Vector3 (Random.value * 24f - 12f, Random.value, Random.value* 24f - 12f);
+			}
+		}
+	}
+}
