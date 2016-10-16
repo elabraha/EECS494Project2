21746574acf966c8352093dba7ae386508c87e24
Yuxiang Yao <yuxiangy@umich.edu>
2016-10-15 23:35:35 -0400

    a destructible cube, which may used in our design, also a test for building a destructible player so that when time is up or it encounters an enemy, it may be crushed rather than a pure reload or dead scene

---
+++ Assets/_Script/Broken/BreakControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Player") {
+			this.transform.FindChild ("Cube").gameObject.SetActive (false);
+			Destroy (gameObject.GetComponent<BoxCollider> ());
+			foreach (Rigidbody rgd in gameObject.GetComponentsInChildren<Rigidbody>()) {
+				rgd.constraints = RigidbodyConstraints.None;
+				rgd.velocity = new Vector3 (Random.value * 10f - 5f, Random.value, Random.value* 10f - 5f);
+			}
+		}
+	}
+}
